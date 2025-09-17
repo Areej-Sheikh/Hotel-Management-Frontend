d842f05d80d5e9d2cd72d555b467d2777e2f77a2
Areej-Sheikh <areejfatimasheikh25@gmail.com>
2025-09-17 17:06:29 +0530

    toast location changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import {store} from "./store/store.jsx";
+import { store } from "./store/store.jsx";
 import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -11,15 +11,16 @@ createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <BrowserRouter>
       <App />
+
       <ToastContainer
-        position="top-left"
+        position="bottom-right"
         autoClose={2000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
         rtl={false}
         theme="light"
-      />
+      ></ToastContainer>
     </BrowserRouter>
   </Provider>
 );
